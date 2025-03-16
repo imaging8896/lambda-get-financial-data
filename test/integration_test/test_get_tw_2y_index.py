ddb648b9cd53cbfe64c74ead6b8e33cd97db3c3d
Ted Chen <imaging8896@gmail.com>
2025-03-16 15:24:22 +0800

    test: Update test data of getting TW 2y index

--- test/integration_test/test_get_tw_2y_index.py
+++ test/integration_test/test_get_tw_2y_index.py
@@ -7,7 +7,7 @@ from data import get
 
 @pytest.mark.parametrize(
     "expect_price", [
-        {'date': date(2025, 2, 19).isoformat(), 'opening': '23589.44', 'highest': '23683.46', 'lowest': '23550.99', 'closing': '23604.08', 'volume': '389394000000'}
+        {'date': date(2025, 3, 14).isoformat(), 'opening': '21986.81', 'highest': '22074.03', 'lowest': '21895.9', 'closing': '21968.05', 'volume': '348017000000', 'margin_financing_balance': '325695000000', 'short_selling_amount': '235795000', 'day_trading_amount': '6402000'}
 ])
 def test_get_tw_2y_index(expect_price):
     results = get("tw_2y_index")
